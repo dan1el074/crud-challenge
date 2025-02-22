e34590910e9a26c8c3a6728fa186e628fe5e7a38
Daniel Vargas <daniel.rvargas10@gmail.com>
2025-02-22 20:14:53 -0300

    initial commit

---
+++ src/test/java/com/example/crud_challenge/CrudChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
